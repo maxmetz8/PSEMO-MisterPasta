e0195c1d55c7405841f267da85e6349457bcbe19
Sukhi-2003 <94359466+Sukhi-2003@users.noreply.github.com>
2024-10-07 10:39:36 +0200

    feature/added cypress to repo

---
+++ misterpasta.client/cypress/support/commands.ts
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       login(email: string, password: string): Chainable<void>
+//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+//     }
+//   }
+// }
