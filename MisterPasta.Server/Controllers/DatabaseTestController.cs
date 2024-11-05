705ce724844c54cf46e84488de24591f9bd71f61
MaxMetz <maxmetz8@gmail.com>
2024-11-05 10:23:12 +0100

    database test

---
+++ MisterPasta.Server/Controllers/DatabaseTestController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using MisterPasta.Server;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("[controller]")]
+public class DatabaseTestController : ControllerBase
+{
+	private readonly MyDbContext _context;
+
+	public DatabaseTestController(MyDbContext context)
+	{
+		_context = context;
+	}
+
+	[HttpGet("test-connection")]
+	public IActionResult TestConnection()
+	{
+		try
+		{
+			// Voer een eenvoudige query uit om de verbinding te testen
+			var canConnect = _context.Database.CanConnect();
+			if (canConnect)
+			{
+				return Ok("Succesvol verbonden met de database!");
+			}
+			else
+			{
+				return StatusCode(500, "Kan geen verbinding maken met de database.");
+			}
+		}
+		catch (Exception ex)
+		{
+			return StatusCode(500, $"Er is een fout opgetreden: {ex.Message}");
+		}
+	}
+}
