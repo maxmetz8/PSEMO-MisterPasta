fa2594055d3ced5785ad6b1fa8b8d90342ff24db
Sukhi-2003 <94359466+Sukhi-2003@users.noreply.github.com>
2024-11-15 12:17:28 +0100

    feature-#41/get-products: added method endpoints for productscontroller

---
+++ MisterPasta.Server/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MisterPasta.Server.DTO;
+using MisterPasta.Server.Models;
+using MisterPasta.Server.Services;
+
+namespace MisterPasta.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductService _productService;
+        private readonly FilterService  _filterService;
+
+        public ProductsController(ProductService productService, FilterService filterService)
+        {
+            _productService = productService;
+            _filterService = filterService;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetProducts()
+        {
+            var products = await _productService.GetProducts();
+            return Ok(products);
+        }
+
+        // GET: api/Products/id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDTO>> GetProduct(int id)
+        {
+            return await _productService.GetProductById(id);
+        }
+
+        [HttpPost("Filter")]
+        public async Task<IEnumerable<ProductDTO>> Search(ProductRequestDTO? productRequest)
+        {
+            if (productRequest != null)
+            {
+                return await _filterService.Filter(productRequest);
+            } else
+            {
+                // betere oplossing vinden
+                return null;
+            }
+        }
+
+    }
+}
---
+++ MisterPasta.Server/Migrations/20241113201615_AddedLargeDescription.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MisterPasta.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedLargeDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LargeDescription",
+                table: "Products",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "LargeDescription",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "LargeDescription",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "LargeDescription",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4,
+                column: "LargeDescription",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 5,
+                column: "LargeDescription",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 6,
+                column: "LargeDescription",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 7,
+                column: "LargeDescription",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 8,
+                column: "LargeDescription",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 9,
+                column: "LargeDescription",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 10,
+                column: "LargeDescription",
+                value: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LargeDescription",
+                table: "Products");
+        }
+    }
+}
